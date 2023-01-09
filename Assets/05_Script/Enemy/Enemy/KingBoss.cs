b90cd76ac175f58ddcd55bee00db3c0d9f007c98
dkdkdsa <bos95330@gmail.com>
2023-01-10 01:32:17 +0900

    보스가 세마리

---
+++ Assets/05_Script/Enemy/Enemy/KingBoss.cs
@@ -0,0 +1,83 @@
+using Classs;
+using FD.Dev;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingBoss : EnemyRoot
+{
+
+    [SerializeField] private Transform summonPos;
+
+    public override void Chase()
+    {
+
+
+        int n = Random.Range(0, 101);
+
+        if (n == 100 && !attackCoolDown)
+        {
+
+            ChangeAIState("Attack");
+
+        }
+
+    }
+
+    public override void Attack()
+    {
+
+        base.Attack();
+
+        StartEnemyAttack(() =>
+        {
+
+            float r = 60;
+
+            for (int i = 0; i <5; i++)
+            {
+
+                r -= 20;
+                FAED.Pop("SuReGum", transform.position, Quaternion.Euler(new Vector3(0, 0, r))).GetComponent<SuReGum>().SetBullet();
+
+            }
+
+            FAED.InvokeDelay(() =>
+            {
+
+                for(int i = 0; i < 3; i++)
+                {
+
+                    FAED.Pop("Bat", new Vector2(Random.Range(-2f, 2f), Random.Range(-2f, 2f)) + (Vector2)summonPos.position, Quaternion.identity);
+
+                }
+
+            }, 0.5f);
+
+            FAED.InvokeDelay(() =>
+            {
+
+                for (int i = 0; i < 3; i++)
+                {
+
+                    FAED.Pop("Bat", new Vector2(Random.Range(-2f, 2f), Random.Range(-2f, 2f)) + (Vector2)summonPos.position, Quaternion.identity);
+
+                }
+
+                float r = 60;
+
+                for (int i = 0; i < 5; i++)
+                {
+
+                    r -= 20;
+                    FAED.Pop("SuReGum", transform.position, Quaternion.Euler(new Vector3(0, 0, r))).GetComponent<SuReGum>().SetBullet();
+
+                }
+
+            }, 1.5f);
+
+        });
+
+    }
+
+}
---
+++ Assets/05_Script/Enemy/Enemy/MonsterBoss.cs
@@ -0,0 +1,45 @@
+using Classs;
+using FD.Dev;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBoss : EnemyRoot
+{
+
+    [SerializeField] private Transform summonPos;
+
+    public override void Chase()
+    {
+
+        int n = Random.Range(0, 101);
+
+        if (n == 100 && !attackCoolDown)
+        {
+
+            ChangeAIState("Attack");
+
+        }
+
+    }
+
+    public override void Attack()
+    {
+
+        base.Attack();
+
+        StartEnemyAttack(() =>
+        {
+
+            for(int i = 0; i < 8; i++)
+            {
+
+                FAED.Pop("Bat", new Vector2(Random.Range(-3f, 3f), Random.Range(-3f, 3f)) + (Vector2)summonPos.position, Quaternion.identity);
+
+            }
+
+        });
+
+    }
+
+}
