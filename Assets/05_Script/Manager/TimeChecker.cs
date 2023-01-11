8d6a5916189615b72959d7f10f8309f4b5360d4a
jimJunhyun <tsumego2015@gmail.com>
2023-01-11 22:01:33 +0900

    endscene and time

--- Assets/05_Script/Manager/TimeChecker.cs
+++ Assets/05_Script/Manager/TimeChecker.cs
@@ -5,26 +5,47 @@ using TMPro;
 
 public class TimeChecker : MonoBehaviour
 {
-	float startTime = 0;
+	[SerializeField]
+	bool isShower = false;
 	float endTime = 0;
 	TextMeshProUGUI txt;
 	private void Awake()
 	{
-
+		if (isShower)
+		{
+			On();
+		}
 	}
-
 	public void RecordStart()
 	{
-		startTime = Time.time;
+		PlayerPrefs.SetFloat("START", Time.realtimeSinceStartup);
+		Debug.Log(Time.realtimeSinceStartup);
+		
 	}
 	public void RecordEnd()
 	{
-		endTime = Time.time;
-		txt.text = $"{endTime - startTime} √  µµ∏¡√∆¥Ÿ!";
-		txt.enabled = true;
+		PlayerPrefs.SetFloat("END", Time.realtimeSinceStartup);
+		Debug.Log(Time.realtimeSinceStartup);
 	}
-	public void Off()
+	public void On()
 	{
-		txt.enabled = false;
+		txt = GetComponentInChildren<TextMeshProUGUI>();
+		float sec = Mathf.Round(PlayerPrefs.GetFloat("END", 0) - PlayerPrefs.GetFloat("START", 0));
+		int hour = (int)sec / 3600;
+		sec %= 3600;
+		int min = (int)sec / 60;
+		sec %= 60;
+		string showTxt = "";
+		if(hour > 0)
+		{
+			showTxt += $"{hour} Ω√∞£ ";
+		}
+		if (min > 0)
+		{
+			showTxt += $"{min} ∫– ";
+		}
+		showTxt += $"{sec} √  µµ∏¡√∆¥Ÿ!";
+		txt.text = showTxt;
+		txt.enabled = true;
 	}
 }
